c86c1976e06e62195f6380b72adeefd1b03fdbf6
sudip_raj23 <sudipto.raj23@gmail.com>
2022-10-13 21:53:01 +0530

    fixed bugs

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,11 @@ public static void main(String args[]){
 System.out.println("hello world by me")
 }
 
-<<<<<<< HEAD
 public void newMethod(){
     System.out.println("new method by sid")
 }
-=======
 public void add(int a, int b){
 int c = a + b;
->>>>>>> a68660b9cd0ee9059086ce43c7047197a416e322
 }
 }
 
