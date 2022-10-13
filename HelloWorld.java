580ec91a4689679e62ef97eea3d10c6a3ff416a1
sudip_raj23 <sudipto.raj23@gmail.com>
2022-10-13 21:21:56 +0530

    new method added by sid

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld{
 public static void main(String args[]){
 System.out.println("hello world by me")
 }
+
+public void newMethod(){
+    System.out.println("new method by sid")
+}
 }
